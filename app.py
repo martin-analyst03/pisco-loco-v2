5bee1c207173f6f3e74c9e2beb29c10e23f94891
martin-analyst03 <droguettmartin16@gmail.com>
2026-08-05 12:49:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ with tab2:
             with st.spinner("La IA está analizando los datos..."):
                 try:
                     genai.configure(api_key=api_key)
-                    model = genai.GenerativeModel('models/gemini-1.5-flash')
+                    model = genai.GenerativeModel('gemini-1.5-flash-latest')
                     datos_str = df.to_string(index=False)
 
                     prompt = f"""
